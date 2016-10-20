4600275e44af2c92c708dca0033c49d58b8cf64c
Administrator <Administrator@172.31.22.230>
2016-10-20 09:33:49 +0000

    changes made for testing

--- RichSpring/src/com/example/customer/bean/CustomerBean.java
+++ RichSpring/src/com/example/customer/bean/CustomerBean.java
@@ -15,7 +15,7 @@ public class CustomerBean implements Serializable{
 	public CustomerServiceImpl getCustomerService() {
 		return customerService;
 	}
-	//test1
+	//test3
 	public void setCustomerService(CustomerServiceImpl customerService) {
 		this.customerService = customerService;
 	}
